dc186d516ea42da069518ee9ea0465a4929a2d6e
Nicolas Hery <hery.nicolas@gmail.com>
2016-04-26 08:03:16 -0400

    Add simple Scotty endpoint

--- Main.hs
+++ Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import BasicPrelude
 
 import Control.Applicative (liftA)
 import Control.Lens ((&), (.~), (^.))
-import Control.Monad.Reader (ReaderT, runReaderT, asks)
+import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, asks)
 import Data.Aeson (FromJSON(..), ToJSON(..), Value(Object), eitherDecode)
 import Data.Aeson.Types ((.:))
 import Data.Digest.Pure.MD5 (md5)
@@ -15,16 +16,20 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import GHC.Generics (Generic)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wreq (defaults, param, getWith, responseBody)
 import System.Environment (getEnv)
-
-type App a = ReaderT Config IO a
+import Web.Scotty.Trans (ScottyT, ActionT, scottyT, get, text, middleware)
 
 data Config = Config
   { port :: Int
   , marvelPublicKey :: Text
   , marvelPrivateKey :: Text
   } deriving (Show)
+
+newtype ConfigM a = ConfigM
+  { runConfigM :: ReaderT Config IO a
+  } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
 
 getConfig :: IO Config
 getConfig = do
@@ -71,7 +76,7 @@ defaultPaginationOptions = PaginationOptions
 getTimestamp :: IO Integer
 getTimestamp = liftA round getPOSIXTime
 
-createHash :: Integer -> App Text
+createHash :: Integer -> ConfigM Text
 createHash ts = do
   privateKey <- asks marvelPrivateKey
   publicKey <- asks marvelPublicKey
@@ -79,7 +84,7 @@ createHash ts = do
   let apiHash = show (md5 (TLE.encodeUtf8 (TL.fromStrict payload)))
   return apiHash
 
-findAllCharacters :: PaginationOptions -> App (Either String CharactersResponse)
+findAllCharacters :: PaginationOptions -> ConfigM (Either String CharactersResponse)
 findAllCharacters paginationOptions = do
   publicKey <- asks marvelPublicKey
   ts <- liftIO getTimestamp
@@ -95,12 +100,23 @@ findAllCharacters paginationOptions = do
   let result = eitherDecode (r ^. responseBody)
   return result
 
-application :: App ()
+application :: ScottyT TL.Text ConfigM ()
 application = do
-  result <- findAllCharacters defaultPaginationOptions
-  liftIO (print (show result))
+  middleware logStdoutDev
+  get "/" getHome
+  get "/characters" getCharacters
+
+getHome :: ActionT TL.Text ConfigM ()
+getHome = text "hello world"
+
+getCharacters :: ActionT TL.Text ConfigM ()
+getCharacters = do
+  result <- lift (findAllCharacters defaultPaginationOptions)
+  text (TL.fromStrict (show result))
 
 main :: IO ()
 main = do
   config <- getConfig
-  runReaderT application config
+  let _port = port config
+  let runIO m = runReaderT (runConfigM m) config
+  scottyT _port runIO application
